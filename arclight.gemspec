9bb2c9c0146e2b5f6796df4875ecd617e46b251d
Jack Reed <phillipjreed@gmail.com>
2017-04-11 18:07:03 -0400

    add Blacklight and Rails dependencies and use in generator

--- arclight.gemspec
+++ arclight.gemspec
@@ -22,6 +22,9 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ['lib']
 
+  spec.add_dependency 'rails', '>= 4.2.6', '< 6'
+  spec.add_dependency 'blacklight', '~> 6.0'
+
   spec.add_development_dependency 'bundler', '~> 1.14'
   spec.add_development_dependency 'rake', '~> 12.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
--- spec/test_app_templates/lib/generators/test_app_generator.rb
+++ spec/test_app_templates/lib/generators/test_app_generator.rb
@@ -7,6 +7,20 @@ class TestAppGenerator < Rails::Generators::Base
   # into the test app, this generator will be run immediately
   # after setting up the application
 
+  def add_gems
+    gem 'blacklight', '~> 6.0'
+
+    Bundler.with_clean_env do
+      run 'bundle install'
+    end
+  end
+
+  def run_blacklight_generator
+    say_status('warning', 'GENERATING BL', :yellow)
+
+    generate 'blacklight:install', '--devise'
+  end
+
   def install_engine
     generate 'arclight:install'
   end
