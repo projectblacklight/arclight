cea7694a7be95f6e1bf75b68dbf388ca36b6ee8b
Jack Reed <phillipjreed@gmail.com>
2017-04-11 17:56:56 -0400

    add and install engine_cart to application

--- Rakefile
+++ Rakefile
@@ -3,4 +3,6 @@ require "rspec/core/rake_task"
 
 RSpec::Core::RakeTask.new(:spec)
 
+require 'engine_cart/rake_task'
+
 task :default => :spec
---
+++ spec/test_app_templates/lib/generators/test_app_generator.rb
@@ -0,0 +1,13 @@
+require 'rails/generators'
+
+class TestAppGenerator < Rails::Generators::Base
+  source_root './spec/test_app_templates'
+
+  # if you need to generate any additional configuration
+  # into the test app, this generator will be run immediately
+  # after setting up the application
+
+  def install_engine
+    generate 'arclight:install'
+  end
+end
