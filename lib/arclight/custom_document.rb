6a70a6132391d34012f54b15eec81424427a33d1
Darren Hardy <darren.hardy.phd@gmail.com>
2017-04-14 10:37:53 -0700

    add overrides of solr_ead to get different index_as properties

--- lib/arclight/custom_document.rb
+++ lib/arclight/custom_document.rb
@@ -7,9 +7,15 @@ module Arclight
     use_terminology SolrEad::Document
 
     extend_terminology do |t|
-      t.unitid(path: 'archdesc/did/unitid', index_as: [:displayable])
+      t.unitid(path: 'archdesc/did/unitid', index_as: %i[displayable])
       t.repository(path: 'archdesc/did/repository/*/text()', index_as: %i[displayable facetable])
       t.creator(path: "archdesc/did/origination[@label='creator']/*/text()", index_as: %i[displayable facetable])
+
+      # overrides of solr_ead to get different `index_as` properties
+      t.extent(path: 'archdesc/did/physdesc/extent', index_as: %i[displayable])
+      t.unitdate(path: 'archdesc/did/unitdate[not(@type)]', index_as: %i[displayable])
+      t.accessrestrict(path: 'archdesc/accessrestrict/p', index_as: %i[displayable])
+      t.scopecontent(path: 'archdesc/scopecontent/p', index_as: %i[displayable])
     end
   end
 end
