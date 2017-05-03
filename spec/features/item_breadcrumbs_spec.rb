4e685ffb6c2fd9d8433ec9772f87a2b558cc42f2
Jessie Keck <jessie.keck@gmail.com>
2017-05-03 11:56:31 -0700

    Update breadcrumb spec to be more permissive to order.

--- spec/features/item_breadcrumbs_spec.rb
+++ spec/features/item_breadcrumbs_spec.rb
@@ -5,7 +5,11 @@ require 'spec_helper'
 RSpec.describe 'Item breadcrumb', type: :feature do
   it 'results page shows navigable breadcrumbs' do
     visit search_catalog_path q: '', search_field: 'all_fields', per_page: 40
-    within '.document-position-26' do
+    document = page.all('article').find do |doc|
+      doc.all('h3 a', text: 'Phase II: Expansion').present?
+    end
+
+    within document do
       expect(page).to have_css 'a', text: 'Series I: Administrative Records'
       expect(page).to have_css 'a', text: 'Reports'
       expect(page).to have_css 'a', text: 'Expansion Plan'
