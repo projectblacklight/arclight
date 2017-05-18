7dc25a4b9f9ce3dd335749d8d6fc6c0962f3198a
Darren Hardy <drh@stanford.edu>
2017-05-18 12:16:29 -0700

    index more notes to be searchable and displayable, and refactor common terminologies

---
+++ lib/arclight/shared_terminology_behavior.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+module Arclight
+  ##
+  # An extendable mixin intended to share terminology behavior between
+  # the CustomDocument and CustomComponent classes
+  module SharedTerminologyBehavior
+    def add_unitid(t, prefix)
+      t.unitid(path: prefix + 'did/unitid', index_as: %i[displayable searchable])
+    end
+
+    def add_extent(t, prefix)
+      t.extent(path: prefix + 'did/physdesc/extent', index_as: %i[displayable searchable])
+    end
+
+    # date indexing
+    def add_dates(t, prefix)
+      t.normal_unit_dates(path: prefix + 'did/unitdate/@normal')
+      t.unitdate_bulk(path: prefix + 'did/unitdate[@type=\'bulk\']', index_as: %i[displayable])
+      t.unitdate_inclusive(path: prefix + 'did/unitdate[@type=\'inclusive\']', index_as: %i[displayable])
+      t.unitdate_other(path: prefix + 'did/unitdate[not(@type)]', index_as: %i[displayable])
+      t.unitdate(path: prefix + 'did/unitdate', index_as: %i[displayable])
+    end
+
+    def add_searchable_notes(t, prefix) # rubocop: disable Metrics/MethodLength
+      # various searchable notes
+      %i[
+        accessrestrict
+        accruals
+        acqinfo
+        altformavail
+        appraisal
+        arrangement
+        bibliography
+        bioghist
+        custodhist
+        fileplan
+        note
+        odd
+        originalsloc
+        otherfindaid
+        phystech
+        prefercite
+        processinfo
+        relatedmaterial
+        scopecontent
+        separatedmaterial
+        userestrict
+      ].each do |k|
+        t.send(k, path: "#{prefix}#{k}/p", index_as: %i[displayable searchable])
+      end
+
+      # various searchable notes in the did
+      %i[
+        abstract
+        materialspec
+        physloc
+      ].each do |k|
+        t.send(k, path: "#{prefix}did/#{k}", index_as: %i[displayable searchable])
+      end
+      t.did_note(path: "#{prefix}did/note", index_as: %i[displayable searchable]) # conflicts with top-level note
+    end
+  end
+end
--- spec/features/fielded_search_results_spec.rb
+++ spec/features/fielded_search_results_spec.rb
@@ -4,10 +4,40 @@ require 'spec_helper'
 
 RSpec.describe 'Field-based search results', type: :feature do
   describe 'searches by' do
-    it '#all_fields' do
-      visit search_catalog_path q: 'a brief account', search_field: 'all_fields'
-      within('.document-position-0') do
-        expect(page).to have_css '.index_title', text: /A brief account/
+    context '#all_fields' do
+      context 'for collections, fielded content is searchable by' do
+        it 'title' do
+          visit search_catalog_path q: 'student life', search_field: 'all_fields', f: { level_sim: ['Collection'] }
+          within('.document-position-0') do
+            expect(page).to have_css '.index_title', text: /Stanford University student life photograph album/
+          end
+        end
+        it 'biographical note' do
+          visit search_catalog_path q: 'boorishness', search_field: 'all_fields'
+          within('.document-position-0') do
+            expect(page).to have_css '.index_title', text: /Alpha Omega Alpha Archives/
+          end
+        end
+        it 'bioghist note' do
+          visit search_catalog_path q: 'Hippocratic oath', search_field: 'all_fields'
+          within('.document-position-0') do
+            expect(page).to have_css '.index_title', text: /Alpha Omega Alpha Archives/
+          end
+        end
+      end
+      context 'for components, fielded content is searchable by' do
+        it 'title' do
+          visit search_catalog_path q: 'a brief account', search_field: 'all_fields', f: { level_sim: ['File'] }
+          within('.document-position-0') do
+            expect(page).to have_css '.index_title', text: /A brief account/
+          end
+        end
+        it 'userestrict note' do
+          visit search_catalog_path q: 'Original photographs must be handled using gloves', search_field: 'all_fields'
+          within('.document-position-0') do
+            expect(page).to have_css '.index_title', text: /Series VI: Photographs/
+          end
+        end
       end
     end
 
