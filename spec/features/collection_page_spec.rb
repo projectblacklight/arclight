12c3cf3f6a8d0d2858818e94e49817c19e9b489e
Jack Reed <phillipjreed@gmail.com>
2017-05-18 17:11:23 -0400

    breadcrumbs for the collection and component show pages :bread:

--- spec/features/collection_page_spec.rb
+++ spec/features/collection_page_spec.rb
@@ -261,4 +261,13 @@ RSpec.describe 'Collection Page', type: :feature do
       end
     end
   end
+  describe 'breadcrumb' do
+    it 'links home, collection, displays title' do
+      within '.al-show-breadcrumb' do
+        expect(page).to have_css 'a', text: 'Home'
+        expect(page).to have_css 'a', text: 'Collections'
+        expect(page).to have_content 'Alpha Omega Alpha Archives, 1894-1992'
+      end
+    end
+  end
 end
