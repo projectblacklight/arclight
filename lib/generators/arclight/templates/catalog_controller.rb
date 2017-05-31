3299d199b030ddb14c758f5fcfa5c9669f13150f
Camille Villa <cvilla100@gmail.com>
2017-05-31 15:54:41 -0700

    Add tabbed display for digital objects

--- lib/generators/arclight/templates/catalog_controller.rb
+++ lib/generators/arclight/templates/catalog_controller.rb
@@ -299,8 +299,6 @@ class CatalogController < ApplicationController
       config.view_config(:show).document_actions.delete(action)
     end
 
-    # Insert the viewer between the header and the content
-    config.show.partials.insert(1, :arclight_viewer)
     # Insert the breadcrumbs at the beginning
     config.show.partials.unshift(:show_breadcrumbs)
 
--- spec/features/component_page_spec.rb
+++ spec/features/component_page_spec.rb
@@ -7,11 +7,49 @@ RSpec.describe 'Component Page', type: :feature do
 
   before { visit solr_document_path(id: doc_id) }
 
-  describe 'Viewer section' do
-    let(:doc_id) { 'a0011-xmlaspace_ref6_lx4' }
+  describe 'tabbed display' do
+    it 'clicking contents toggles visibility', js: true do
+      expect(page).to have_css '#overview', visible: true
+      expect(page).to have_css '#online-content', visible: false
+      click_link 'Online content'
+      expect(page).to have_css '#overview', visible: false
+      expect(page).to have_css '#online-content', visible: true
+    end
+  end
 
-    it 'renders digital object viewer initialization markup' do
-      expect(page).to have_css('.al-oembed-viewer[data-arclight-oembed-url="http://purl.stanford.edu/kc844kt2526"]')
+  describe 'Viewer section' do
+    context 'embedded content' do
+      let(:doc_id) { 'a0011-xmlaspace_ref6_lx4' }
+
+      it 'renders digital object viewer initialization markup', js: true do
+        expect(page).to have_css(
+          '.al-oembed-viewer[data-arclight-oembed-url="http://purl.stanford.edu/kc844kt2526"]',
+          visible: false
+        )
+        click_link('Online content')
+        expect(page).to have_css(
+          '.al-oembed-viewer[data-arclight-oembed-url="http://purl.stanford.edu/kc844kt2526"]',
+          visible: true
+        )
+      end
+    end
+
+    context 'non-embeddable content' do
+      let(:doc_id) { 'aoa271aspace_843e8f9f22bac69872d0802d6fffbb04' }
+
+      it 'renders a list of links', js: true do
+        click_link('Online content')
+        expect(page).to have_link('Folder of digitized stuff')
+        expect(page).to have_css('a', text: /^Letter from Christian B. Anfinsen/)
+      end
+    end
+
+    context 'no content' do
+      let(:doc_id) { 'aoa271aspace_a951375d104030369a993ff943f61a77' }
+
+      it 'renders disabled tab' do
+        expect(page).to have_css('.nav-link.disabled', text: 'No online content')
+      end
     end
   end
 
