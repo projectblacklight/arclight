6e201939c0c6d11237dd1817d3bc4875ec15e02f
Jessie Keck <jessie.keck@gmail.com>
2018-07-12 11:03:13 -0700

    Update capybara spec to be permissive of new lines in text.

--- spec/features/collection_page_spec.rb
+++ spec/features/collection_page_spec.rb
@@ -265,7 +265,7 @@ RSpec.describe 'Collection Page', type: :feature do
           within '.document-position-0 ' do
             expect(page).to have_css(
               '.al-document-abstract-or-scope',
-              text: /^SCOPE AND CONTENTS Administrative records include/i
+              text: /^SCOPE AND CONTENTS.*Administrative records include/im
             )
           end
         end
