1be145133a53ea943f534dd51c0b816b11ecbc72
Chris Beer <cabeer@stanford.edu>
2019-08-15 13:15:47 -0700

    Add enough fields to get the pages to load without errors

--- lib/arclight/traject/ead2_config.rb
+++ lib/arclight/traject/ead2_config.rb
@@ -2,6 +2,9 @@ require 'traject'
 require 'traject/nokogiri_reader'
 require 'traject_plus'
 require 'traject_plus/macros'
+require 'arclight/normalized_date'
+require 'arclight/normalized_title'
+require 'active_support/core_ext/array/wrap'
 
 extend TrajectPlus::Macros
 
@@ -9,12 +12,26 @@ settings do
   provide "nokogiri.namespaces",  {
     "xmlns" => "urn:isbn:1-931666-22-9",
   }
+  provide "solr_writer.commit_on_close", "true"
 end
 
-# Top level 
+# Top level
 to_field 'id', extract_xpath("//xmlns:eadid"), strip, gsub('.', '-')
 to_field 'title_ssm', extract_xpath("//xmlns:archdesc/xmlns:did/xmlns:unittitle")
 to_field 'title_teim', extract_xpath("//xmlns:archdesc/xmlns:did/xmlns:unittitle")
+to_field 'ead_ssi' do |record, accumulator, context|
+  accumulator << context.output_hash['id'].first
+end
+
+to_field 'unitdate_bulk_ssim', extract_xpath('//xmlns:archdesc/xmlns:did/xmlns:unitdate[@type="bulk"]')
+to_field 'unitdate_inclusive_ssim', extract_xpath('//xmlns:archdesc/xmlns:did/xmlns:unitdate[@type="inclusive"]')
+to_field 'unitdate_other_ssim', extract_xpath('//xmlns:archdesc/xmlns:did/xmlns:unitdate[not(@type)]')
+
+to_field 'normalized_title_ssm' do |record, accumulator, context|
+  dates = Arclight::NormalizedDate.new(context.output_hash['unitdate_inclusive_ssim'], context.output_hash['unitdate_bulk_ssim'], context.output_hash['unitdate_other_ssim']).to_s
+  title = context.output_hash['title_ssm'].first
+  accumulator << Arclight::NormalizedTitle.new(title, dates).to_s
+end
 
 # Each component child document
 # <c> <c01> <c12>
@@ -22,9 +39,28 @@ compose 'components', ->(record, accumulator, context) { accumulator.concat reco
   to_field 'id' do |record, accumulator, context|
     accumulator << [
       context.clipboard[:parent].output_hash['id'],
-      record.attribute('id')&.value
+      record.attribute('id')&.value&.strip&.gsub('.', '-')
     ].join('')
   end
+
+  to_field 'ead_ssi' do |record, accumulator, context|
+    accumulator << context.output_hash['id'].first
+  end
+
+  to_field 'title_ssm', extract_xpath("./xmlns:did/xmlns:unittitle")
+  to_field 'title_teim', extract_xpath("./xmlns:did/xmlns:unittitle")
+
+  to_field 'unitdate_bulk_ssim', extract_xpath('./xmlns:did/xmlns:unitdate[@type="bulk"]')
+  to_field 'unitdate_inclusive_ssim', extract_xpath('./xmlns:did/xmlns:unitdate[@type="inclusive"]')
+  to_field 'unitdate_other_ssim', extract_xpath('./xmlns:did/xmlns:unitdate[not(@type)]')
+
+  to_field 'normalized_title_ssm' do |record, accumulator, context|
+    dates = Arclight::NormalizedDate.new(context.output_hash['unitdate_inclusive_ssim'], context.output_hash['unitdate_bulk_ssim'], context.output_hash['unitdate_other_ssim']).to_s
+    title = context.output_hash['title_ssm'].first
+    accumulator << Arclight::NormalizedTitle.new(title, dates).to_s
+  end
+
+  to_field 'component_level_isim', literal(0)
 end
 
 
