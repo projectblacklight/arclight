c920150715cfba3036ac675b71c3100facde28db
Esty Thomas <esty@umich.edu>
2019-08-16 16:41:11 -0400

    Check whether iFrame has loaded before loading it

--- app/assets/javascripts/arclight/oembed_viewer.js
+++ app/assets/javascripts/arclight/oembed_viewer.js
@@ -11,6 +11,13 @@ Blacklight.onLoad(function () {
 
     $viewerElements.each(function (i, element) {
       var $el = $(element);
+      var possible_iframe = $(element).find('iframe')
+      if (possible_iframe && possible_iframe.length > 0) {
+        var iframe = possible_iframe[0]
+        if (iframe.contentWindow) {
+            return
+        }
+      }
       var data = $el.data();
       var resourceUrl = data.arclightOembedUrl;
       $.ajax({
