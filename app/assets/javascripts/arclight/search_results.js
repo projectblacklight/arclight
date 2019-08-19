82471956096a1072a8e03a90021505fb1248500f
Esty Thomas <esty@umich.edu>
2019-08-19 14:42:38 -0400

    Fix eslint errors in javascripts

--- app/assets/javascripts/arclight/search_results.js
+++ app/assets/javascripts/arclight/search_results.js
@@ -1,15 +1,15 @@
 Blacklight.onLoad(function () {
   'use strict';
 
-  var histogram_content = $('#al-date-range-histogram-content')
-  var show_span = $('#al-date-range-histogram-show')
-  var hide_span = $('#al-date-range-histogram-hide')
-  histogram_content.on('show.bs.collapse', function() {
-    show_span.attr('hidden', 'hidden')
-    hide_span.removeAttr('hidden')
-  })
-  histogram_content.on('hide.bs.collapse', function() {
-    hide_span.attr('hidden', 'hidden')
-    show_span.removeAttr('hidden')
-  })
+  var histogramContent = $('#al-date-range-histogram-content');
+  var showSpan = $('#al-date-range-histogram-show');
+  var hideSpan = $('#al-date-range-histogram-hide');
+  histogramContent.on('show.bs.collapse', function () {
+    showSpan.attr('hidden', 'hidden');
+    hideSpan.removeAttr('hidden');
+  });
+  histogramContent.on('hide.bs.collapse', function () {
+    hideSpan.attr('hidden', 'hidden');
+    showSpan.removeAttr('hidden');
+  });
 });
