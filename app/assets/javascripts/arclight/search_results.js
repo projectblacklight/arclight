eb0534f4e0be49b914ced4edd82b270a2a01529d
Esty Thomas <esty@umich.edu>
2019-08-19 09:37:42 -0400

    Show/hide histogram label swaps show and hide with expand/collapse

---
+++ app/assets/javascripts/arclight/search_results.js
@@ -0,0 +1,15 @@
+Blacklight.onLoad(function () {
+    'use strict';
+
+    var histogram_content = $('#al-date-range-histogram-content')
+    var show_span = $('#al-date-range-histogram-show')
+    var hide_span = $('#al-date-range-histogram-hide')
+    histogram_content.on('show.bs.collapse', function() {
+        show_span.attr('hidden', 'hidden')
+        hide_span.removeAttr('hidden')
+    })
+    histogram_content.on('hide.bs.collapse', function() {
+        hide_span.attr('hidden', 'hidden')
+        show_span.removeAttr('hidden')
+    })
+});
