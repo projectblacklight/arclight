673300af7e37bfe5f6065abfa44d9b5a8a390b51
Jack Reed <phillipjreed@gmail.com>
2019-09-05 16:10:22 -0600

    Update display of grouped collection fixes #702

--- spec/features/grouped_results_spec.rb
+++ spec/features/grouped_results_spec.rb
@@ -6,8 +6,9 @@ RSpec.describe 'Grouped search results', type: :feature do
   it 'displays collection group information' do
     visit search_catalog_path q: 'alpha', group: 'true'
     within '.al-grouped-title-bar' do
-      expect(page).to have_css 'h3 a', text: /Alpha/
+      expect(page).to have_css 'h3', text: /Alpha/
       expect(page).to have_css '.al-document-abstract-or-scope', text: /founded in 1902/
+      expect(page).to have_css '.badge', text: '15.0 linear feet (36 boxes + oversize folder)'
     end
     within '.grouped-documents' do
       expect(page).to have_css 'article', count: 3
