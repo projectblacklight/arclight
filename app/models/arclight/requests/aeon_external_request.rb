14dff5dedb9fcdaa50708e56c09145c227cdc9ca
Esty Thomas <esty@umich.edu>
2019-09-16 14:36:17 -0400

    Rubocop AeonExternalRequest and add url_params to it

--- app/models/arclight/requests/aeon_external_request.rb
+++ app/models/arclight/requests/aeon_external_request.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 module Arclight
   module Requests
+    ##
+    # This class should be used to turn configuration into a URL and
+    # POST form specifically aimed at Aeon's external request
+    # endpoint (https://support.atlas-sys.com/hc/en-us/articles/360011820054-External-Request-Endpoint)
     class AeonExternalRequest
       def initialize(document, presenter)
         @document = document
@@ -8,7 +14,7 @@ module Arclight
       end
 
       def url
-        "#{@config['request_url']}/aeon.dll?Action=11&Type=200"
+        "#{@config['request_url']}#{url_params}"
       end
 
       def form_mapping
@@ -21,10 +27,18 @@ module Arclight
 
       def dynamic_mappings
         mappings = {}
-        @config['request_mappings']['accessor'].each_pair do | k, v |
+        @config['request_mappings']['accessor'].each_pair do |k, v|
           mappings[k] = @document.send(v.to_sym)
         end
         mappings
+      end
+
+      def url_params
+        params = []
+        @config['request_mappings']['url_params'].each_pair do |k, v|
+          params.append("#{k}=#{v}")
+        end
+        "?#{params.join('&')}" unless params.empty?
       end
     end
   end
