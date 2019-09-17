8ae5590991246454e61f03e24a8dc2d32d02d46b
Jack Reed <phillipjreed@gmail.com>
2019-09-17 10:46:55 -0600

    Remove page params for grouping/ungrouping

--- app/helpers/arclight_helper.rb
+++ app/helpers/arclight_helper.rb
@@ -110,11 +110,11 @@ module ArclightHelper
   end
 
   def search_with_group
-    search_state.params_for_search.merge('group' => 'true')
+    search_state.params_for_search.merge('group' => 'true').reject { |k| k == 'page' }
   end
 
   def search_without_group
-    search_state.params_for_search.reject { |k| k == 'group' }
+    search_state.params_for_search.reject { |k| %w[group page].include? k }
   end
 
   def search_within_collection(collection_name, search)
