08ce9952e9718a1e0d58e18120f8ca42cf890bcc
Jack Reed <phillipjreed@gmail.com>
2019-09-20 14:49:53 -0600

    Update functionality to insert the list elements in the correct places

--- app/assets/javascripts/arclight/context_navigation.js
+++ app/assets/javascripts/arclight/context_navigation.js
@@ -34,14 +34,35 @@ class ContextNavigation {
       var $doc = $(resp);
       var newDocs = $doc.find('#documents')
         .find('article')
-        .toArray().map(el => new NavigationDocument(el).render());
+        .toArray().map(el => new NavigationDocument(el));
 
-      const docIndex = newDocs.findIndex(doc => doc.id === that.data.arclight.parentsParent)
-      console.log(docIndex, that.data.arclight);
-
+      const originalDocumentIndex = newDocs.findIndex(doc => doc.id === that.data.arclight.originalDocument);
       that.parentLi.find('.al-hierarchy-placeholder').remove();
-      that.parentLi.before(newDocs.join('')).fadeIn(500);
-      // that.el.hide().html(newDocs.join('')).fadeIn(500);
+
+      // Case where this is the sibling tree of the current document
+      if (originalDocumentIndex !== -1) {
+        newDocs[originalDocumentIndex].setAsHighlighted();
+        that.parentLi.before(newDocs.map(newDoc => newDoc.render()).join('')).fadeIn(500);
+      } else {
+      // Case where this is a parent list and needs to be filed correctly
+        const parentIndex = that.data.arclight.originalParents.indexOf(that.data.arclight.parent);
+        const currentId = `${that.data.arclight.originalParents[0]}${that.data.arclight.originalParents[parentIndex + 1]}`;
+        const newDocIndex = newDocs.findIndex(doc => doc.id === currentId);
+
+        // Update the docs before the item
+        // TODO: Add a class or something that doesn't display these until the expando is clicked
+        const beforeDocs = newDocs.slice(0, newDocIndex);
+        that.parentLi.before(beforeDocs.map(newDoc => newDoc.render()).join('')).fadeIn(500);
+        // Update the item
+        const $itemDoc = $(newDocs.slice(newDocIndex, newDocIndex + 1).map(doc => doc.render()).join(''));
+        // Update the id, add classes of the classes. Prepend the current children. 
+        that.parentLi.attr('id', $itemDoc.attr('id'));
+        that.parentLi.addClass($itemDoc.attr('class'));
+        that.parentLi.prepend($itemDoc.children()).fadeIn(500);
+        // Update the docs after the item
+        const afterDocs = newDocs.slice(newDocIndex + 1, newDocs.length);
+        that.parentLi.after(afterDocs.map(newDoc => newDoc.render()).join('')).fadeIn(500);
+      }
     });
   }
 }
@@ -51,8 +72,12 @@ class NavigationDocument {
     this.el = $(el);
   }
   
-  id() {
+  get id() {
     return this.el.find('[data-document-id]').data().documentId;
+  }
+
+  setAsHighlighted() {
+    this.el.find('li.al-collection-context').addClass('al-hierarchy-highlight')
   }
 
   render() {
