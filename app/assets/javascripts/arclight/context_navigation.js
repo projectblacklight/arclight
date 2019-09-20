6668a690d3649403a895e6dea31b143c8510d66e
Jack Reed <phillipjreed@gmail.com>
2019-09-20 15:09:39 -0600

    Move view update to a new method

--- app/assets/javascripts/arclight/context_navigation.js
+++ app/assets/javascripts/arclight/context_navigation.js
@@ -29,41 +29,44 @@ class ContextNavigation {
         search_field: this.data.arclight.search_field,
         view: 'collection_context'
       }
-    }).done(function (response) {
-      var resp = $.parseHTML(response);
-      var $doc = $(resp);
-      var newDocs = $doc.find('#documents')
-        .find('article')
-        .toArray().map(el => new NavigationDocument(el));
+    }).done((response) => that.updateView(response));
+  }
+  
+  updateView(response) {
+    const that = this;
+    var resp = $.parseHTML(response);
+    var $doc = $(resp);
+    var newDocs = $doc.find('#documents')
+      .find('article')
+      .toArray().map(el => new NavigationDocument(el));
 
-      const originalDocumentIndex = newDocs.findIndex(doc => doc.id === that.data.arclight.originalDocument);
-      that.parentLi.find('.al-hierarchy-placeholder').remove();
+    const originalDocumentIndex = newDocs.findIndex(doc => doc.id === that.data.arclight.originalDocument);
+    that.parentLi.find('.al-hierarchy-placeholder').remove();
 
-      // Case where this is the sibling tree of the current document
-      if (originalDocumentIndex !== -1) {
-        newDocs[originalDocumentIndex].setAsHighlighted();
-        that.parentLi.before(newDocs.map(newDoc => newDoc.render()).join('')).fadeIn(500);
-      } else {
-      // Case where this is a parent list and needs to be filed correctly
-        const parentIndex = that.data.arclight.originalParents.indexOf(that.data.arclight.parent);
-        const currentId = `${that.data.arclight.originalParents[0]}${that.data.arclight.originalParents[parentIndex + 1]}`;
-        const newDocIndex = newDocs.findIndex(doc => doc.id === currentId);
+    // Case where this is the sibling tree of the current document
+    if (originalDocumentIndex !== -1) {
+      newDocs[originalDocumentIndex].setAsHighlighted();
+      that.parentLi.before(newDocs.map(newDoc => newDoc.render()).join('')).fadeIn(500);
+    } else {
+    // Case where this is a parent list and needs to be filed correctly
+      const parentIndex = that.data.arclight.originalParents.indexOf(that.data.arclight.parent);
+      const currentId = `${that.data.arclight.originalParents[0]}${that.data.arclight.originalParents[parentIndex + 1]}`;
+      const newDocIndex = newDocs.findIndex(doc => doc.id === currentId);
 
-        // Update the docs before the item
-        // TODO: Add a class or something that doesn't display these until the expando is clicked
-        const beforeDocs = newDocs.slice(0, newDocIndex);
-        that.parentLi.before(beforeDocs.map(newDoc => newDoc.render()).join('')).fadeIn(500);
-        // Update the item
-        const $itemDoc = $(newDocs.slice(newDocIndex, newDocIndex + 1).map(doc => doc.render()).join(''));
-        // Update the id, add classes of the classes. Prepend the current children. 
-        that.parentLi.attr('id', $itemDoc.attr('id'));
-        that.parentLi.addClass($itemDoc.attr('class'));
-        that.parentLi.prepend($itemDoc.children()).fadeIn(500);
-        // Update the docs after the item
-        const afterDocs = newDocs.slice(newDocIndex + 1, newDocs.length);
-        that.parentLi.after(afterDocs.map(newDoc => newDoc.render()).join('')).fadeIn(500);
-      }
-    });
+      // Update the docs before the item
+      // TODO: Add a class or something that doesn't display these until the expando is clicked
+      const beforeDocs = newDocs.slice(0, newDocIndex);
+      that.parentLi.before(beforeDocs.map(newDoc => newDoc.render()).join('')).fadeIn(500);
+      // Update the item
+      const $itemDoc = $(newDocs.slice(newDocIndex, newDocIndex + 1).map(doc => doc.render()).join(''));
+      // Update the id, add classes of the classes. Prepend the current children. 
+      that.parentLi.attr('id', $itemDoc.attr('id'));
+      that.parentLi.addClass($itemDoc.attr('class'));
+      that.parentLi.prepend($itemDoc.children()).fadeIn(500);
+      // Update the docs after the item
+      const afterDocs = newDocs.slice(newDocIndex + 1, newDocs.length);
+      that.parentLi.after(afterDocs.map(newDoc => newDoc.render()).join('')).fadeIn(500);
+    }
   }
 }
 
