64820a7aef2c3f3f220adf493a7756bd8e3941e0
labradford <leaann@notch8.com>
2019-10-02 08:59:50 -0700

    make grouped title a link

--- spec/features/grouped_results_spec.rb
+++ spec/features/grouped_results_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Grouped search results', type: :feature do
   it 'displays collection group information' do
     visit search_catalog_path q: 'alpha', group: 'true'
     within '.al-grouped-title-bar' do
-      expect(page).to have_css 'h3', text: /Alpha/
+      expect(page).to have_css 'h3 a', text: /Alpha/
       expect(page).to have_css '.al-document-abstract-or-scope', text: /founded in 1902/
       expect(page).to have_css '.badge', text: '15.0 linear feet (36 boxes + oversize folder)'
     end
@@ -49,7 +49,7 @@ RSpec.describe 'Grouped search results', type: :feature do
       visit search_catalog_path q: 'alpha', group: 'true', view: 'compact'
 
       within '.al-grouped-title-bar' do
-        expect(page).to have_css 'h3', text: /Alpha/
+        expect(page).to have_css 'h3 a', text: /Alpha/
         expect(page).not_to have_css '.al-document-abstract-or-scope', text: /founded in 1902/
         expect(page).to have_css '.badge', text: '15.0 linear feet (36 boxes + oversize folder)'
       end
