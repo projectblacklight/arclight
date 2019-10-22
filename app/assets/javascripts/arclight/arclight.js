3979f85c52e94a61562fb763cc0229f7731fd87d
Jack Reed <mejackreed@users.noreply.github.com>
2019-10-22 12:25:28 -0600

    Remove unused search_results histogram JavaScript, related to #8… (#969)

--- app/assets/javascripts/arclight/arclight.js
+++ app/assets/javascripts/arclight/arclight.js
@@ -2,7 +2,6 @@
 //= require arclight/collection_navigation
 //= require arclight/context_navigation
 //= require arclight/oembed_viewer
-//= require arclight/search_results
 //= require arclight/truncator
 
 // Vendor Scripts
