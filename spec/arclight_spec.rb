aefae5b8f03b4753dc57a1aa5f4d0cfd10f20eb2
Justin Coyne <jcoyne@justincoyne.com>
2022-10-06 13:11:19 -0500

    Move the configuration from the initializer to the catalog

--- spec/arclight_spec.rb
+++ spec/arclight_spec.rb
@@ -6,12 +6,4 @@ RSpec.describe Arclight do
   it 'has a version number' do
     expect(Arclight::VERSION).not_to be_nil
   end
-
-  describe 'Custom CatalogController field accessors' do
-    subject(:custom_fields) do
-      Arclight::Engine.config.catalog_controller_field_accessors
-    end
-
-    it { expect(custom_fields).to include :summary_field, :access_field }
-  end
 end
