2f8fd330c39825ac16d5feda217f3e09242079b1
Justin Coyne <jcoyne@justincoyne.com>
2022-10-10 14:14:43 -0500

    Change the interaction for the autocomplete

--- spec/features/autocomplete_spec.rb
+++ spec/features/autocomplete_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe 'Autocomplete', type: :feature, js: true do
       visit '/catalog'
       find_by_id('q').send_keys 'by-laws'
       expect(page).to have_content 'amendments to articles'
+      send_keys(:arrow_down)
       send_keys(:tab)
       expect(page).to have_field('search for', with: 'amendments to articles of incorporation and revised constitution and              by-laws, 1960')
     end
