798949f7fa2b88336679c83d1b7d6950af47e502
Justin Coyne <jcoyne@justincoyne.com>
2022-10-18 15:19:57 -0500

    Refactor oembed script to use a class
    Use fetch instead of jquery-ajax

--- app/assets/javascripts/arclight/oembed_viewer.js
+++ app/assets/javascripts/arclight/oembed_viewer.js
@@ -1,36 +1,50 @@
+class OembedLoader {
+  constructor(el) {
+    this.el = $(el)
+  }
+
+  load() {
+    const loadedAttr = this.el.attr('loaded')
+    var data = this.el.data()
+    var resourceUrl = data.arclightOembedUrl
+    if (loadedAttr && loadedAttr === 'loaded') {
+      return
+    }
+
+    fetch(resourceUrl)
+      .then((response) => response.text())
+      .then((body) => {
+        const oEmbedEndPoint = this.findOEmbedEndPoint(body)
+        if (!oEmbedEndPoint || oEmbedEndPoint.length === 0) {
+          return
+        }
+        this.loadEndPoint(oEmbedEndPoint)
+      })
+  }
+
+  static findOEmbedEndPoint(body) {
+    const template = document.createElement('template')
+    template.innerHTML = body.match(/<link .*>/g).join('') // Parse out link elements so image assets are not loaded
+    return template.querySelector('link[rel="alternate"][type="application/json+oembed"]').getAttribute('href')
+  }
+
+  loadEndPoint(oEmbedEndPoint) {
+    fetch(oEmbedEndPoint)
+      .then((response) => response.text())
+      .then((body) => {
+        this.el.hide()
+          .html(body)
+          .fadeIn(500)
+        this.el.attr('loaded', 'loaded')
+      })
+  }
+}
+
 Blacklight.onLoad(function () {
   'use strict'
 
   document.querySelectorAll('[data-arclight-oembed="true"]').each((element) => {
-    var $el = $(element);
-    var loadedAttr = $el.attr('loaded');
-    var data = $el.data();
-    var resourceUrl = data.arclightOembedUrl;
-    if (loadedAttr && loadedAttr === 'loaded') {
-      return;
-    }
-
-    $.ajax({
-      url: resourceUrl,
-      dataType: 'html'
-    }).done(function (response) {
-      var links = $('<div>' + response.match(/<link .*>/g).join('') + '</div>'); // Parse out link elements so image assets are not loaded
-      var oEmbedEndPoint = links.find('link[rel="alternate"][type="application/json+oembed"]').prop('href');
-
-      if (!oEmbedEndPoint || oEmbedEndPoint.length === 0) {
-        return;
-      }
-
-      $.ajax({
-        url: oEmbedEndPoint
-      }).done(function (oEmbedResponse) {
-        if (oEmbedResponse.html) {
-          $el.hide()
-              .html(oEmbedResponse.html)
-              .fadeIn(500);
-          $el.attr('loaded', 'loaded');
-        }
-      });
-    });
-  });
+    const loader = new OembedLoader(element)
+    loader.load()
+  })
 })
