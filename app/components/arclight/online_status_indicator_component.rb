9ba23b34684bdb114452ec4af37ec5802b344372
Cory Lown <corylown@stanford.edu>
2022-11-08 10:37:14 -0500

    Fix online status component comment

--- app/components/arclight/online_status_indicator_component.rb
+++ app/components/arclight/online_status_indicator_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Arclight
-  # Render an oembed viewer for a document
+  # Render an online status indicator for a document
   class OnlineStatusIndicatorComponent < Blacklight::Component
     def initialize(document:, **)
       @document = document
