4d1e2f9c35375478cdc005a0e00d65ed15f0b7ba
Justin Coyne <jcoyne@justincoyne.com>
2022-11-09 15:24:03 -0600

    Use the rspec progress formatter except when focused on a single file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -48,6 +48,16 @@ RSpec.configure do |config|
     c.syntax = :expect
   end
 
+  # Many RSpec users commonly either run the entire suite or an individual
+  # file, and it's useful to allow more verbose output when running an
+  # individual spec file.
+  if config.files_to_run.one?
+    # Use the documentation formatter for detailed output,
+    # unless a formatter has already been configured
+    # (e.g. via a command-line flag).
+    config.default_formatter = 'doc'
+  end
+
   config.infer_spec_type_from_file_location!
 
   config.include ViewComponent::TestHelpers, type: :component
