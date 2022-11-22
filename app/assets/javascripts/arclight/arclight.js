91d43bf4bd4f35d13bf7ac48a44dbaab45b821c0
Justin Coyne <jcoyne@justincoyne.com>
2022-11-22 15:31:57 -0600

    Highlight the collection context link when it is active
    Fixes #1280

--- app/assets/javascripts/arclight/arclight.js
+++ app/assets/javascripts/arclight/arclight.js
@@ -1,4 +1,7 @@
 import 'arclight/oembed_viewer'
 
 import Truncate from 'arclight/truncate_controller'
+import CollectionLink from 'arclight/collection_link_controller'
+
 Stimulus.register('arclight-truncate', Truncate)
+Stimulus.register('arclight-collection-link', CollectionLink)
