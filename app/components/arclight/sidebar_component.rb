da770b5e338a13b659e246d619239e4540f9ea3b
Nick Budak <budak@stanford.edu>
2023-01-06 13:14:36 -0800

    Add misc. documentation comments
    Placates rubocop.

--- app/components/arclight/sidebar_component.rb
+++ app/components/arclight/sidebar_component.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module Arclight
+  # A sidebar with collection context widget and tools
   class SidebarComponent < Blacklight::Document::SidebarComponent
     delegate :blacklight_config, :document_presenter, :should_render_field?,
              :turbo_frame_tag, to: :helpers
--- lib/arclight/routes.rb
+++ lib/arclight/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module Arclight
+  # Custom routes for Arclight
   module Routes
     require 'arclight/routes/hierarchy'
   end
