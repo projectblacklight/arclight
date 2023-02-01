73e946d1586a74a1d892020a36b5d2355015ac47
Marlo Longley <mlongley@stanford.edu>
2023-02-01 17:56:39 -0500

    Make DocumentDownloadComponent more configurable

--- app/components/arclight/sidebar_component.rb
+++ app/components/arclight/sidebar_component.rb
@@ -5,5 +5,15 @@ module Arclight
   class SidebarComponent < Blacklight::Document::SidebarComponent
     delegate :blacklight_config, :document_presenter, :should_render_field?,
              :turbo_frame_tag, to: :helpers
+
+    def collection_context
+      render Arclight::CollectionContextComponent.new(presenter: document_presenter(document), download_component: Arclight::DocumentDownloadComponent)
+    end
+
+    def collection_sidebar
+      render Arclight::CollectionSidebarComponent.new(document: document,
+                                                      collection_presenter: document_presenter(document.collection),
+                                                      partials: blacklight_config.show.metadata_partials)
+    end
   end
 end
