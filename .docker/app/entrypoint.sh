12e86a4ce6c74ef8612ddf1ef0f9596b2d1f6655
Maura C <maura_carbone@harvard.edu>
2023-12-07 15:28:14 -0500

    Bundle install

--- .docker/app/entrypoint.sh
+++ .docker/app/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 rm -f /app/.internal_test_app/tmp/pids/server.pid
+bundle install
 bundle exec rails engine_cart:generate
 bundle install
-#sleep 1000000000
 exec bundle exec rake arclight:server["-p 3000 -b 0.0.0.0"]
