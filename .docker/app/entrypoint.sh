f62e8dff0d1f95153e0515dd0d10863198d6801a
Maura C <maura_carbone@harvard.edu>
2023-12-08 12:17:38 -0500

    Entrypoint with seed file

--- .docker/app/entrypoint.sh
+++ .docker/app/entrypoint.sh
@@ -4,5 +4,5 @@ set -e
 rm -f /app/.internal_test_app/tmp/pids/server.pid
 bundle install
 bundle exec rails engine_cart:generate
-bundle install
-exec bundle exec rake arclight:server["-p 3000 -b 0.0.0.0"]
+bundle exec rails arclight:seed
+bundle exec rake arclight:server["-p 3000 -b 0.0.0.0"]
