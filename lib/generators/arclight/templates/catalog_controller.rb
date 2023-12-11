86675bbed70271ef79d56b182c10f3bef4a61684
Greg Wiedeman <gregory.wiedeman1@gmail.com>
2023-12-11 14:49:16 -0500

    added locales and examples and changed solr fields for missing elements

--- lib/generators/arclight/templates/catalog_controller.rb
+++ lib/generators/arclight/templates/catalog_controller.rb
@@ -280,7 +280,7 @@ class CatalogController < ApplicationController
     config.add_background_field 'phystech', field: 'phystech_html_tesm', helper_method: :render_html_tags
     config.add_background_field 'physloc', field: 'physloc_html_tesm', helper_method: :render_html_tags
     config.add_background_field 'materialspec', field: 'materialspec_tesim', helper_method: :render_html_tags
-    config.add_background_field 'fileplan', field: 'fileplan_tesim', helper_method: :render_html_tags
+    config.add_background_field 'fileplan', field: 'fileplan_html_tesim', helper_method: :render_html_tags
     config.add_background_field 'descrules', field: 'descrules_ssm', helper_method: :render_html_tags
 
     # Collection Show Page - Related Section
@@ -289,7 +289,7 @@ class CatalogController < ApplicationController
     config.add_related_field 'otherfindaid', field: 'otherfindaid_html_tesm', helper_method: :render_html_tags
     config.add_related_field 'altformavail', field: 'altformavail_html_tesm', helper_method: :render_html_tags
     config.add_related_field 'originalsloc', field: 'originalsloc_html_tesm', helper_method: :render_html_tags
-    config.add_related_field 'odd', field: 'odd_tesim', helper_method: :render_html_tags
+    config.add_related_field 'odd', field: 'odd_html_tesim', helper_method: :render_html_tags
 
     # Collection Show Page - Indexed Terms Section
     config.add_indexed_terms_field 'access_subjects', field: 'access_subjects_ssim', link_to_facet: true, separator_options: {
@@ -335,10 +335,10 @@ class CatalogController < ApplicationController
     config.add_component_field 'phystech', field: 'phystech_html_tesm', helper_method: :render_html_tags
     config.add_component_field 'materialspec', field: 'materialspec_tesim', helper_method: :render_html_tags
     config.add_component_field 'physloc', field: 'physloc_html_tesm', helper_method: :render_html_tags
-    config.add_component_field 'fileplan', field: 'fileplan_tesim', helper_method: :render_html_tags
-    config.add_component_field 'altformavail', field: 'altformavail_tesim', helper_method: :render_html_tags
+    config.add_component_field 'fileplan', field: 'fileplan_html_tesim', helper_method: :render_html_tags
+    config.add_component_field 'altformavail', field: 'altformavail_html_tesim', helper_method: :render_html_tags
     config.add_component_field 'otherfindaid', field: 'otherfindaid_html_tesm', helper_method: :render_html_tags
-    config.add_component_field 'odd', field: 'odd_tesim', helper_method: :render_html_tags
+    config.add_component_field 'odd', field: 'odd_html_tesim', helper_method: :render_html_tags
 
     # Component Show Page - Indexed Terms Section
     config.add_component_indexed_terms_field 'access_subjects', field: 'access_subjects_ssim', link_to_facet: true, separator_options: {
