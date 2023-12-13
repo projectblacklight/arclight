512d70081700e6dc630c0ed021ead57c4fc82aaa
Sean Aery <sean.aery@duke.edu>
2023-12-12 20:37:37 -0500

    Refactor Parent.global_id so identifier format only has to be configured in one place. Advances #1318 - Capture the concatenated/formatted IDs at indexing time, in parent_ids_ssim array - Use that data instead of global_id

--- spec/components/arclight/breadcrumb_component_spec.rb
+++ spec/components/arclight/breadcrumb_component_spec.rb
@@ -5,8 +5,8 @@ require 'spec_helper'
 RSpec.describe Arclight::BreadcrumbComponent, type: :component do
   let(:document) do
     SolrDocument.new(
-      parent_ssim: %w[def ghi],
-      parent_unittitles_ssm: %w[DEF GHI],
+      parent_ids_ssim: %w[abc123 abc123_def abc123_ghi],
+      parent_unittitles_ssm: %w[ABC123 DEF GHI],
       ead_ssi: 'abc123',
       repository_ssm: 'my repository'
     )
@@ -29,7 +29,8 @@ RSpec.describe Arclight::BreadcrumbComponent, type: :component do
 
     it 'renders only that many breadcrumb links' do
       expect(rendered).to have_selector 'li', text: 'my repository'
-      expect(rendered).to have_link 'DEF', href: '/catalog/abc123_def'
+      expect(rendered).to have_link 'ABC123', href: '/catalog/abc123'
+      expect(rendered).not_to have_link 'DEF', href: '/catalog/abc123_def'
       expect(rendered).not_to have_link 'GHI', href: '/catalog/abc123_ghi'
     end
 
@@ -43,7 +44,8 @@ RSpec.describe Arclight::BreadcrumbComponent, type: :component do
 
     it 'skips some breadcrumb links' do
       expect(rendered).not_to have_selector 'li', text: 'my repository'
-      expect(rendered).not_to have_link 'DEF', href: '/catalog/abc123_def'
+      expect(rendered).not_to have_link 'ABC123', href: '/catalog/abc123'
+      expect(rendered).to have_link 'DEF', href: '/catalog/abc123_def'
       expect(rendered).to have_link 'GHI', href: '/catalog/abc123_ghi'
     end
   end
