982c42ce69e65034b24eaf6314808bf182364c5b
Cory Lown <corylown@stanford.edu>
2025-05-08 09:46:27 -0400

    Extract an expand hierarchy button component

---
+++ app/components/arclight/expand_hierarchy_button_component.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Arclight
+  # Component for rendering an expand button in the hierarchy view
+  class ExpandHierarchyButtonComponent < Blacklight::Component
+    def initialize(path:, classes: 'btn btn-secondary btn-sm')
+      super
+      @path = path
+      @classes = classes
+    end
+
+    def expand_link
+      link_to t('arclight.views.show.expand'), @path, class: @classes
+    end
+  end
+end
